da35a59d2b1ba919718fe00a639edcbb4e0febec
Dany Shaanan <danyshaanan@gmail.com>
2016-07-03 11:51:42 +0300

    Cleanup

--- unitTest.js
+++ unitTest.js
@@ -1,42 +1,30 @@
+'use strict'
 
-var _ = require('lodash');
-var requirejs = require('requirejs');
-var Jasmine = require('jasmine-core');
-//
-//requirejs.define('jasmine-boot', ['jasmine-core'], function(jasmine) {
-//    return jasmine.boot;
-//});
+const _ = require('lodash')
+const requirejs = require('requirejs')
+const Jasmine = require('jasmine-core')
+const JasmineConsoleReporter = require('jasmine-console-reporter')
 
-var jasmine = Jasmine.boot(Jasmine);
+const jasmine = Jasmine.boot(Jasmine)
 
-requirejs.define('specs', [], function() {
-    describe('specs', function () {
-        it('should run', function () {
-            expect(1).toBe(1);
-        })
-    });
-});
-
-var JasmineConsoleReporter = require('jasmine-console-reporter');
-var reporter = new JasmineConsoleReporter({
-    colors: 1,           // (0|false)|(1|true)|2
-    cleanStack: 1,       // (0|false)|(1|true)|2|3
-    verbosity: 4,        // (0|false)|1|2|(3|true)|4
-    listStyle: 'indent', // "flat"|"indent"
+const reporter = new JasmineConsoleReporter({
+    colors: 1,
+    cleanStack: 1,
+    verbosity: 4,
+    listStyle: 'indent',
     activity: false
-});
+})
 
-jasmine.getEnv().addReporter(reporter);
+jasmine.getEnv().addReporter(reporter)
 
+requirejs.define('specs', [], () => {
+    describe('specs', () => {
+        it('should run', () => {
+            expect(1).toBe(1)
+        })
+    })
+})
 
-requirejs(['specs'], function (specs) {
-    // jasmine.getEnv().execute();
-
-
+requirejs(['specs'], specs => {
     jasmine.getEnv().execute()
-    //jasmine.ConsoleReporter({
-    //    print:function() {
-    //        process.stdout.write(util.format.apply(this, arguments));
-    //    }
-    //}))
-});
+})
